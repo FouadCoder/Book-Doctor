da99a1f535ec527a0a5a99a152d89a8a6c4af64b
Dark <157938552+Mahmoud504A@users.noreply.github.com>
2024-09-21 15:43:28 +0300

    Delete book_doctor/lib/payment/strip_keys.dart

--- book_doctor/lib/payment/strip_keys.dart
+++
@@ -1,5 +0,0 @@
-abstract class PaymentKeys {
-  static const secretKeyPay = "sk_test_51Pj5odP9cBkm4zZ6spG5CVDUTPh2M34fwoTg57A3yOctrdWB5m4NTioqQhaGC6NQgtXQGd1UpzgG13YC7Nm72Mph005VjDiLDA";
-  static const apiPublish = "pk_test_51Pj5odP9cBkm4zZ6xw6FueQp1NYPGWA1pSjzOAMGgoNPmayOeXCD0AayUqspwpNDKPxD0i7tQR5Py7sBeU0TBJfv00q1wERbky";
-  static const apiChatAi = "AIzaSyAoYJ6O3ZnG_VU_3Wke9QQTxBaNeOp6Rio";
-}
